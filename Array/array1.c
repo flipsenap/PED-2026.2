a1aecbab1e8a326930391ef253821ecfc63b5610
Filipe Pedrosa <104914049+flipsenap@users.noreply.github.com>
2026-09-09 22:01:04 -0300

    Add files via upload

---
+++ Array/array1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+void reorganizar(int *a, int *b) {
+    if (*a < *b) {
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+}
+
+int main() {
+    int x, y;
+
+    printf("Digite o primeiro valor inteiro: ");
+    scanf("%d", &x);
+
+    printf("Digite o segundo valor inteiro: ");
+    scanf("%d", &y);
+
+    reorganizar(&x, &y);
+
+    printf("Maior valor: %d\n", x);
+    printf("Menor valor: %d\n", y);
+
+    return 0;
+}
